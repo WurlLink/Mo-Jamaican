d37c932b9bc13b9bf7a1d4c7bebad6fdba938f8e
Shannon Robinson <PimpMachine@Shannons-MacBook-Air.local>
2018-07-08 09:44:02 -0400

    first commit

---
+++ Mo Jamaican/Mo Jamaican/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mo Jamaican
+//
+//  Created by Shannon Robinson on 7/8/18.
+//  Copyright © 2018 Wurl Link. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
