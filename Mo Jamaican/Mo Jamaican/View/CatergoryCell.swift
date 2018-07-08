1d06e8f8be259cfeabd019f64fe2ef4ea54a4085
Shannon Robinson <PimpMachine@Shannons-MacBook-Air.local>
2018-07-08 15:33:10 -0400

    Cells Working

---
+++ Mo Jamaican/Mo Jamaican/View/CatergoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CatergoryCell.swift
+//  Mo Jamaican
+//
+//  Created by Shannon Robinson on 7/8/18.
+//  Copyright © 2018 Wurl Link. All rights reserved.
+//
+
+import UIKit
+
+class CatergoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
